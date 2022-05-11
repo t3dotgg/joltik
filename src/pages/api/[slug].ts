8711b5cf122a1b516ccf2b92e92f0a24b404955c
theo <me@t3.gg>
2022-05-11 16:45:09 -0700

    working kinda

---
+++ src/pages/api/[slug].ts
@@ -0,0 +1,33 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+import { prisma } from "../../db/client";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const slug = req.query["slug"];
+
+  if (!slug || typeof slug !== "string") {
+    res.statusCode = 404;
+
+    res.send(JSON.stringify({ message: "pls use with a slug" }));
+
+    return;
+  }
+
+  const data = await prisma.shortLink.findFirst({
+    where: {
+      slug: {
+        equals: slug,
+      },
+    },
+  });
+
+  if (!data) {
+    res.statusCode = 404;
+
+    res.send(JSON.stringify({ message: "slug not found" }));
+
+    return;
+  }
+
+  return res.redirect(data.url);
+};
