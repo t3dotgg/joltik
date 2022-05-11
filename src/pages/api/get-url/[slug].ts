826cbec38d6e15023481ae1e026fa7eebd80a0ed
theo <me@t3.gg>
2022-05-11 16:58:08 -0700

    Cache headers

--- src/pages/api/get-url/[slug].ts
+++ src/pages/api/get-url/[slug].ts
@@ -24,6 +24,13 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (!data) {
     res.statusCode = 404;
 
+    res.setHeader("Content-Type", "application/json");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Cache-Control",
+      "s-maxage=1000000000, stale-while-revalidate"
+    );
+
     res.send(JSON.stringify({ message: "slug not found" }));
 
     return;
